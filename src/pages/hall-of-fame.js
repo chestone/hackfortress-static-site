4c3f760d97d236ec5850068eb1524951b0798270
Cheston Lee <chestonlloyd@gmail.com>
2018-01-04 21:50:15 -0500

    updating hof

--- src/pages/hall-of-fame.js
+++ src/pages/hall-of-fame.js
@@ -1,18 +1,57 @@
 import React from 'react'
 
 const HallOfFamePage = () => (
-  <article>
-    <section>
+  <article className="hof__article">
+    <section className="hof__list">
       <h2>Shmoocon</h2>
       <ul>
         <li>
           2009 - The Shmoobs
         </li>
+        <li>
+          2010 - Mobile Disco
+        </li>
+        <li>
+          2011 - Penn State
+        </li>
+        <li>
+          2012 - Penn State/Pwn State
+        </li>
+        <li>
+          2013 - Team Zelda
+        </li>
+        <li>
+          2014 - Jolly & Friends
+        </li>
+        <li>
+            2015 - Mobile Disco
+        </li>
       </ul>
     </section>
-    <section>
+    <section className="hof__list">
       <h2>DEFCON</h2>
       <ul>
+        <li>
+          2009 - Name of winning team has been lost to the ages
+        </li>
+        <li>
+          2010 - No HackFortress :(
+        </li>
+        <li>
+          2011 - Team Zelda
+        </li>
+        <li>
+          2012 - Team Zelda
+        </li>
+        <li>
+          2013 - Jolly & Friends
+        </li>
+        <li>
+          2014 - Jolly & Friends
+        </li>
+        <li>
+          2015 - Team Not Ready For This
+        </li>
       </ul>
     </section>
   </article>
