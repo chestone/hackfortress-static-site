ad285ca8d7a7c5b8a70b5aabd6399ee42d584302
Cheston Lee <chestonlloyd@gmail.com>
2018-01-14 15:37:57 -0500

    carousel fixes

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,31 +9,12 @@ const flickityOptions = {
   initialIndex: 2
 }
 
-const Carousel = () => (
+const Carousel = ({children}) => (
   <Flickity
     className="carousel"
     options={flickityOptions}
   >
-    <img 
-      className="carousel-cell"
-      src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-3B54Xjm/0/4a3541e8/M/NOT_READY_FOR_THIS-M.jpg" 
-      alt="Winner Shmoocon 2016" 
-      />
-    <img 
-      className="carousel-cell"
-      src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-qzX7rdf/0/8a8fdf41/M/DEFCON2017-M.jpg" 
-      alt="Winner DEFCON 2017" 
-      />
-    <img 
-      className="carousel-cell"
-      src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-wLGqzMh/0/6c839ff4/M/hf-shmoo-2016-M.jpg" 
-      alt="Winner DEFCON 2015" 
-      />
-    <img 
-      className="carousel-cell"
-      src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-PKzhzMQ/0/25f6aa7d/M/NOTREADYFORTHIS2015-M.jpg" 
-      alt="Winner Team Not Ready For This" 
-      />
+    {children}
   </Flickity>
 )
 
--- src/pages/hall-of-fame.js
+++ src/pages/hall-of-fame.js
@@ -4,7 +4,28 @@ import Carousel from '../components/Carousel/Carousel'
 const HallOfFamePage = () => (
   <div>
     <h2>Past Winners of the Golden Wrench</h2>
-    <Carousel />
+    <Carousel>
+      <img 
+        className="carousel-cell"
+        src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-3B54Xjm/0/4a3541e8/M/NOT_READY_FOR_THIS-M.jpg" 
+        alt="Winner Shmoocon 2016" 
+        />
+      <img 
+        className="carousel-cell"
+        src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-qzX7rdf/0/8a8fdf41/M/DEFCON2017-M.jpg" 
+        alt="Winner DEFCON 2017" 
+        />
+      <img 
+        className="carousel-cell"
+        src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-wLGqzMh/0/6c839ff4/M/hf-shmoo-2016-M.jpg" 
+        alt="Winner DEFCON 2015" 
+        />
+      <img 
+        className="carousel-cell"
+        src="https://photos.smugmug.com/Events/Hackfortress-Photos/i-PKzhzMQ/0/25f6aa7d/M/NOTREADYFORTHIS2015-M.jpg" 
+        alt="Winner Team Not Ready For This" 
+        />
+    </Carousel>
     <article className="hof__article">
       <section className="hof__list">
         <h2>Shmoocon</h2>
